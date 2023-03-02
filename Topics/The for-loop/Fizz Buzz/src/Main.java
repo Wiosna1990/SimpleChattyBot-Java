bbfcccb201fb8cb9f6b5367cf7a857197e161079
Wiosna1990 <zanzarah@op.pl>
2023-03-02 19:27:53 +0100

    git remote add origin git@github.com:Wiosna1990/SimpleChattyBot-Java.git git branch -M main git push -u origi main

---
+++ Topics/The for-loop/Fizz Buzz/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        for(int i = number1; i<=number2; i++){
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }else if(i%5==0){
+                System.out.println("Buzz");
+            }else if(i%3==0){
+                System.out.println("Fizz");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+}
